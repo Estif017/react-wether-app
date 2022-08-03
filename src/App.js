3440897d11cc528f2e20689c63eac69d74603244
Estifanos Teklearegay <estifoteklearegay@gmail.com>
2022-08-03 02:41:03 -0700

    restarting from the scratch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import { Routes, Route } from 'react-router-dom';
-import NavBar from './component/navbar';
 import './App.css';
-import CurrentCity from './component/current city';
-import Cities from './component/cities';
 
 const App = () => {
 	return (
 		<div className='App'>
-			<NavBar />
-			<Routes>
-				<Route path='/' element={<CurrentCity />} />
-				<Route path={`cities/:cityName`} element={<Cities />} />
-			</Routes>
+			<h1>hello</h1>
 		</div>
 	);
 };
